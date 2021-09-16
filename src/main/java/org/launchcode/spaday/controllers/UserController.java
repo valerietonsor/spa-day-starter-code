820cb40b56996d783b305f544e6e9e206c7db077
vtonsor <30914395+vtonsor@users.noreply.github.com>
2021-09-15 19:59:18 -0500

    commit step 1

---
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -0,0 +1,15 @@
+package org.launchcode.spaday.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    @GetMapping("add")
+    public String displayAddUserForm(){
+        return "user/add";
+  }
+}
