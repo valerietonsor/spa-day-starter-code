40bf058a47e36944acf9938d577a22af3ed2f386
vtonsor <30914395+vtonsor@users.noreply.github.com>
2021-09-17 17:19:03 -0500

    commit not working bonues

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,15 +1,37 @@
 package org.launchcode.spaday.controllers;
 
+import org.launchcode.spaday.data.UserData;
+import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
+
     @GetMapping("add")
     public String displayAddUserForm(){
         return "user/add";
   }
+
+  @PostMapping()
+    public String processAddUserForm(Model model, @ModelAttribute User newUser, @RequestParam String verify) {
+        // add form submission handling code here
+      String error = "Passwords must match.";
+      String email = newUser.getEmail();
+      String username = newUser.getUsername();
+      model.addAttribute("error", error);
+      model.addAttribute("username", username);
+      model.addAttribute("email", newUser.getEmail());
+
+        if (newUser.getPassword().equals(verify)) {
+            UserData.add(newUser);
+            return "user/index";
+        }
+        else {
+            return "user/add";
+        }
+    }
 }
--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -4,11 +4,15 @@ public class User {
     private String username;
     private String password;
     private String email;
+    private int id;
+    private int nextId = 0;
 
     public User(String username, String password, String email) {
         this.username = username;
         this.password = password;
         this.email = email;
+        this.id = nextId;
+        nextId++;
     }
 
     public String getUsername() {
@@ -34,4 +38,9 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public int getId() {
+        return id;
+    }
+
 }
